64b4b4e46f1900a7a4b3e6ca890583217b2f8894
Andrew Fomera <andrew@zerlex.net>
2019-01-26 16:49:37 -0600

    Add page about tmsg being closed

---
+++ pages/projects/tmsg.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row mt-5">
+    <div class="col-md-12 align-items-center justify-content-center d-flex">
+      <div>
+        <h3 class="text-center">tmsg.io is now closed</h3>
+        <h5 class="text-center text-muted mb-3">tmsg.io was an app for text-messaging groups.</h5>
+
+        <div class="my-3 card card-body">
+          <p class="card-text">
+            tmsg.io was an app I created while I was working a non-software job to make my job easier, when trying to message my coworkers.
+          </p>
+          <p class="card-text">
+            It featured an ability for group messaging, scheduled texts at specific times and managing a contact list. You can read more about it on the introduction blog post
+            <a href="/posts/introducing-tmsg-io">here.</a>
+
+            The code can be found on <a
+              href="https://github.com/king601/MessageMe"
+              target="_blank">GitHub.</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'tmsg.io - Andrew Fomera'
+    }
+  }
+}
+</script>
